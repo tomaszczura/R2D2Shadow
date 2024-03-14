356a834ece6a0bde0c88997f7752fc89a449878b
Tomasz Czura <tom.czura@gmail.com>
2024-03-14 13:48:11 +0100

    Set sounds to play

--- Sound/Sound.ino
+++ Sound/Sound.ino
@@ -4,11 +4,46 @@
 #define Version_Byte 0xFF
 #define Command_Length 0x06
 #define End_Byte 0xEF
-#define Acknowledge 0x00 //Returns info with command 0x41 [0x01: info, 0x00: no info]
+#define Acknowledge 0x00 // Returns info with command 0x41 [0x01: info, 0x00: no info]
 
 SoftwareSerial mySerial(9, 10);
 
+// Commands from arduino
+#define START_SOUND "P1\r";
+#define LEFT_PAD_CON "P2\r"
+#define RIGHT_PAD_CON "P3\r"
+#define L1_UP_ARROW "P4\r"
+#define L1_LEFT_ARROW "P5\r"
+#define L1_DOWN_ARROW "P6\r"
+#define L1_RIGHT_ARROW "P7\r"
+#define L2_UP_ARROW "P8\r"
+#define L2_LEFT_ARROW "P9\r"
+#define L2_RIGHT_ARROW "P10\r"
+#define L2_DOWN_ARROW "P11\r"
+#define UP_ARROW "P12\r"
+#define LEFT_ARROW "P13\r"
+#define RIGHT_ARROW "P14\r"
+
 #define CONTROL_LED 3 // pin 5 on ATMEGA
+
+// - Start - 14 msc
+// - Left controller setup - 32
+// - Right controller setup - 33
+
+// - l1 + arrow
+//  - up - alarm - 13-16
+//  - left - misc - 17-24
+//  - right - ooh - 25-31
+//  - down - sent - 32-53
+// - l2 + arrow
+//   - up  - 1 scream
+//  - left - 2 chortle
+//  - right - 4 wolf
+//  - down - 8 annoyed
+
+//  - up -  3 - doodoo
+//  - left - 6 short
+//  - riught - 7 - patrol
 
 // DFPlayer commands
 #define INITIALIZE_PLAYER 0x3F
@@ -17,88 +52,133 @@ SoftwareSerial mySerial(9, 10);
 #define PLAY_NEXT 0x01
 #define SET_VOLUME 0x06
 
-void setup () {
+// there is a weird offset in the track numbers - 5th track is 1st track
+#define OFFSET 5
+
+void setup()
+{
   pinMode(CONTROL_LED, OUTPUT);
-  mySerial.begin (9600);
-  delay(1000);
+  mySerial.begin(9600);
+  Serial.begin(9600);
   initializePlayer();
-  playFirst();
+  playTrack(14 + OFFSET); // play initial sound
 }
 
-int currentPlay = 1;
-
-void loop () { 
+void loop()
+{
+  delay(500);
   digitalWrite(CONTROL_LED, HIGH);
-  playTrack(currentPlay);
-  currentPlay++;
-  delay(5000);
-  digitalWrite(CONTROL_LED, LOW);
-  playTrack(currentPlay);
-  currentPlay++;
-  delay(5000);
+
+  if (Serial.available() > 0)
+  {
+    String command = Serial.readStringUntil("\r");
+
+    if (command == LEFT_PAD_CON)
+    {
+      playTrack(32 + OFFSET);
+    }
+    else if (command == RIGHT_PAD_CON)
+    {
+      playTrack(33 + OFFSET);
+    }
+    else if (command == L1_UP_ARROW)
+    {
+      playTrack(random(13, 17) + OFFSET);
+    }
+    else if (command == L1_LEFT_ARROW)
+    {
+      playTrack(random(17, 25) + OFFSET);
+    }
+    else if (command == L1_DOWN_ARROW)
+    {
+      playTrack(random(25, 32) + OFFSET);
+    }
+    else if (command == L1_RIGHT_ARROW)
+    {
+      playTrack(random(32, 54) + OFFSET);
+    }
+    else if (command == L2_UP_ARROW)
+    {
+      playTrack(1 + OFFSET);
+    }
+    else if (command == L2_LEFT_ARROW)
+    {
+      playTrack(2 + OFFSET);
+    }
+    else if (command == L2_RIGHT_ARROW)
+    {
+      playTrack(4 + OFFSET);
+    }
+    else if (command == L2_DOWN_ARROW)
+    {
+      playTrack(8 + OFFSET);
+    }
+    else if (command == UP_ARROW)
+    {
+      playTrack(3 + OFFSET);
+    }
+    else if (command == LEFT_ARROW)
+    {
+      playTrack(6 + OFFSET);
+    }
+    else if (command == RIGHT_ARROW)
+    {
+      playTrack(7 + OFFSET);
+    }
+  }
 }
 
-void initializePlayer() {
+void initializePlayer()
+{
   execute_CMD(INITIALIZE_PLAYER, 0, 0);
   delay(500);
   setVolume(20);
   delay(500);
 }
 
-void playTrack(int track) {
+void playTrack(int track)
+{
   execute_CMD(PLAY_TRACK, 0, track);
   delay(500);
 }
 
-void playFirst() {
-  execute_CMD(0x11,0,1); 
+void pause()
+{
+  execute_CMD(PAUSE, 0, 0);
   delay(500);
 }
 
-void pause() {
-  execute_CMD(PAUSE,0,0);
+void play()
+{
+  execute_CMD(0x0D, 0, 1);
   delay(500);
 }
 
-void play() {
-  execute_CMD(0x0D,0,1); 
-  delay(500);
-}
-
-void playNext() {
-  execute_CMD(PLAY_NEXT,0,1);
-  delay(500);
-}
-
-void playPrevious() {
-  execute_CMD(0x02,0,1);
-  delay(500);
-}
-
-void setVolume(int volume) {
+void setVolume(int volume)
+{
   execute_CMD(SET_VOLUME, 0, volume); // Set the volume (0x00~0x30)
   delay(2000);
 }
 
 // Excecute the command and parameters
-void execute_CMD(byte CMD, byte Par1, byte Par2){
+void execute_CMD(byte CMD, byte Par1, byte Par2)
+{
   // Calculate the checksum (2 bytes)
   word checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2);
   // Build the command line
   byte Command_line[10] = {
-    Start_Byte,
-    Version_Byte, 
-    Command_Length, 
-    CMD, 
-    Acknowledge,
-    Par1, 
-    Par2, 
-    highByte(checksum), 
-    lowByte(checksum), End_Byte
-  };
-  //Send the command line to the module
-  for (byte k=0; k<10; k++){
+      Start_Byte,
+      Version_Byte,
+      Command_Length,
+      CMD,
+      Acknowledge,
+      Par1,
+      Par2,
+      highByte(checksum),
+      lowByte(checksum), End_Byte};
+  // Send the command line to the module
+  for (byte k = 0; k < 10; k++)
+  {
     mySerial.write(Command_line[k]);
   }
 }
-
